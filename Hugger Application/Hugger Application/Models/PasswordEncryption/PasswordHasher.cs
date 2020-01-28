bce8850e2f20f79546de9252a75498842e1a20f9
Hubert Haznar <32836807+HubHaz@users.noreply.github.com>
2020-01-28 15:14:05 +0100

    Add password encrypter and decrypter

---
+++ Hugger Application/Hugger Application/Models/PasswordEncryption/PasswordHasher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Hugger_Application.Models.PasswordEncryption
+{
+    public class PasswordHasher
+    {
+        public static string PasswordHash(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            Console.WriteLine(Convert.ToBase64String(hashBytes));
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public static void Decrypter(string password)
+        {
+            string savedPasswordHash = PasswordHash("password");
+            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 16] != hash[i])
+                    throw new UnauthorizedAccessException();
+
+            Console.WriteLine("password matches");
+        }
+    }
+}
