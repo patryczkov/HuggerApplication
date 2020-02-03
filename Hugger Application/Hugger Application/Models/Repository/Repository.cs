9e36d64b8d0585cd3f43e3031fd7cd7119bfcfdc
Hubert Haznar <32836807+HubHaz@users.noreply.github.com>
2020-02-03 21:53:40 +0100

    Create Repository interface and his implementation

---
+++ Hugger Application/Hugger Application/Models/Repository/Repository.cs
@@ -0,0 +1,41 @@
+﻿using Hugger_Web_Application.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Hugger_Application.Models.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        //private readonly IUnitOfWork _unitOfWork;
+        ApplicationContext ApplicationContext;
+
+        public Repository(ApplicationContext applicationContext)
+        {
+            ApplicationContext = applicationContext;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return ApplicationContext.Set<T>().AsNoTracking();
+        }
+
+        public void Create(T entity)
+        {
+            ApplicationContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            ApplicationContext.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            ApplicationContext.Set<T>().Update(entity);
+        }
+        public void Save()
+        {
+            ApplicationContext.SaveChanges();
+        }
+}
+}
