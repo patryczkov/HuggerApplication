c98ce040c04f96ebe876792a0cb2a9b1b5c95fe1
Hubert Haznar <32836807+HubHaz@users.noreply.github.com>
2020-02-03 21:54:19 +0100

    Create interface and implement UserRepository

---
+++ Hugger Application/Hugger Application/Models/Repository/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Hugger_Web_Application.Models;
+
+namespace Hugger_Application.Models.Repository
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        User GetUserByID(int userID);
+    }
+}
