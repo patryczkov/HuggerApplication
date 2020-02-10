4314eb5811a8490a8fedcf12cd8e478ce11a2fd0
Patryczkov <patryczkov@gmail.com>
2020-02-10 12:11:28 +0100

    Add new properties to userEntity and userPreferanceEntity

--- Hugger Application/Hugger Application/Data/Entities/UserPreference.cs
+++ Hugger Application/Hugger Application/Data/Entities/UserPreference.cs
@@ -8,7 +8,7 @@ namespace Hugger_Web_Application.Models
 {
     public class UserPreference
     {
-       
+        public string Value { get; set; }
         public int UserId { get; set; }
         public virtual User User { get; set; }
         public int PreferenceId { get; set; }
