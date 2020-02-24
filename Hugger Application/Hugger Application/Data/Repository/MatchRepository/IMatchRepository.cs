9caebff5cf741a35872a9b8c0aa870571a7aae4f
Patryczkov <patryczkov@gmail.com>
2020-02-24 10:22:25 +0100

    Create IMatchRepository and it's implementation

---
+++ Hugger Application/Hugger Application/Data/Repository/MatchRepository/IMatchRepository.cs
@@ -0,0 +1,17 @@
+﻿using Hugger_Application.Models.Repository;
+using Hugger_Web_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hugger_Application.Data.Repository.MatchRepository
+{
+    interface IMatchRepository : IRepository<Match>
+    {
+        Task<Match[]> GetAllMatchesAsync();
+        Task<Match[]> GetMatchesByUserIdAsync(int userId);
+        Task<Match[]> GetMatchesByDateOfMatch(string date);
+
+    }
+}
---
+++ Hugger Application/Hugger Application/Data/Repository/MatchRepository/MatchRepository.cs
@@ -0,0 +1,56 @@
+﻿
+using Hugger_Application.Models.Repository;
+using Hugger_Web_Application.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hugger_Application.Data.Repository.MatchRepository
+{
+    public class MatchRepository : Repository<Match>, IMatchRepository
+    {
+        private readonly UserContext _context;
+        private readonly ILogger _logger;
+
+        public MatchRepository(UserContext userContext, ILogger logger) : base(userContext)
+        {
+            _context = userContext;
+            _logger = logger;
+        }
+
+        public async Task<Match[]> GetAllMatchesAsync()
+        {
+            _logger.LogInformation("Getting all matches");
+
+            IQueryable<Match> matchesQuery = _context.Matches;
+
+            return await matchesQuery.ToArrayAsync();
+
+
+        }
+
+        public async Task<Match[]> GetMatchesByUserIdAsync(int userId)
+        {
+            _logger.LogInformation($"Getting matches for userId= {userId}");
+            
+            IQueryable<Match> matchesQuery = _context.Matches;
+            matchesQuery.Where(mtch => mtch.UserIDSender == userId || mtch.UserIDReceiver == userId);
+
+            return await matchesQuery.ToArrayAsync();
+
+
+        }
+
+
+        public Task<Match[]> GetMatchesByDateOfMatch(string date)
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+    }
+}
