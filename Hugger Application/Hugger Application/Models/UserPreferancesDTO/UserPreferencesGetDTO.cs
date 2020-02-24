f19bc1bec3ef92f5cb097a11bab7488ecdbd8846
Patryczkov <patryczkov@gmail.com>
2020-02-24 14:35:37 +0100

    Create UserPreferencesGetDTO.cs

---
+++ Hugger Application/Hugger Application/Models/UserPreferancesDTO/UserPreferencesGetDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hugger_Application.Models.UserPreferancesDTO
+{
+    public class UserPreferencesGetDTO
+    {
+        public int UserId { get; set; }
+        public string PreferenceName { get; set; }
+        public string Value { get; set; }
+    }
+}
