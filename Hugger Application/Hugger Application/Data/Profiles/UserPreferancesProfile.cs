00c817226be2e2aeb72b13211d281635a92d9447
Patryczkov <patryczkov@gmail.com>
2020-02-24 15:43:21 +0100

    Create UserPreferancesProfile.cs

---
+++ Hugger Application/Hugger Application/Data/Profiles/UserPreferancesProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Hugger_Application.Models.UserPreferancesDTO;
+using Hugger_Web_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hugger_Application.Data.Profiles
+{
+    public class UserPreferancesProfile: Profile
+    {
+        public UserPreferancesProfile()
+        {
+            CreateMap<UserPreference, UserPreferencesGetDTO>()
+                .ReverseMap();
+        }
+    }
+}
