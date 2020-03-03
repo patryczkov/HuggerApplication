ab098d440caebc08585f31d436e520c20ec9d730
Patryczkov <patryczkov@gmail.com>
2020-03-03 14:44:20 +0100

    Add DTOs for UserChar, rename UserPreference folder

---
+++ Hugger Application/Hugger Application/Models/UserCharacteristicDTO/UserCharCreateDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hugger_Application.Models.UserCharacteristicDTO
+{
+    public class UserCharCreateDTO
+    {
+        public int UserId { get; set; }
+        public int CharacteristicId { get; set; }
+        [Required]
+        [MaxLength(16)]
+        public string Value { get; set; }
+    }
+}
+}
---
+++ Hugger Application/Hugger Application/Models/UserCharacteristicDTO/UserCharGetDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hugger_Application.Models.UserCharacteristicDTO
+{
+    public class UserCharGetDTO
+    {
+        public int UserId { get; set; }
+        public int CharacteristicId { get; set; }
+        public string  CahracteristicName { get; set; }
+        public string  Value { get; set; }
+    }
+}
