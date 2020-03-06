724349ee3cb7eb0738c843d8596f1d182530cb74
Patryk Duda <patryczkov@users.noreply.github.com>
2020-03-06 12:58:10 +0100

    Add message DTOs and message profile fow mapper

---
+++ Hugger Application/Hugger Application/Data/Profiles/MessageProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Hugger_Application.Models.MessageDTO;
+using Hugger_Web_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hugger_Application.Data.Profiles
+{
+    public class MessageProfile :Profile
+    {
+        public MessageProfile()
+        {
+            CreateMap<Message, MessageCreateDTO>()
+                .ReverseMap();
+
+            CreateMap<Message, MessageGetDTO>()
+                .ReverseMap();
+        }
+    }
+}
--- Hugger Application/Hugger Application/Models/MessageDTO/MessageCreateDTO.cs
+++ Hugger Application/Hugger Application/Models/MessageDTO/MessageCreateDTO.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Hugger_Web_Application.Models
+namespace Hugger_Application.Models.MessageDTO
 {
-    public class Message
+    public class MessageCreateDTO
     {
-        [Key]
-        public int Id { get; set; }
-        
         public string TimeOfSend { get; set; }
         public bool WasRead { get; set; } = false;
         [MaxLength(255)]
         public string Content { get; set; }
-
-        [ForeignKey("Matches")]
         public int MatchId { get; set; }
-        public virtual Match Match { get; set; }
-
-
+        public int UserSenderId { get; set; }
     }
 }
